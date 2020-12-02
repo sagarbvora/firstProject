426052a40d6b408dff85d243b5cb4ad192224d79
sagar vora <sagarbvora10@gmail.com>
2020-12-02 14:27:23 +0530

    initial setup data

--- src/Hook_CRUD.js
+++ src/Hook_CRUD.js
@@ -1,7 +1,5 @@
 import React, {useState} from "react";
-import {Col, Row} from "react-bootstrap";
 import Button from "@material-ui/core/Button";
-import "./CrudData.css";
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 
@@ -13,201 +11,130 @@ const numOfCountries = [
     {value: "Africa", label: "Africa"},
     {value: "Pakistan", label: "Pakistan"},
 ];
-const initialData = {
-    firstName: "",
-    lastName: "",
-    age: "",
-    gender: "",
-    address: "",
-    country: "",
-    isActive: "",
-}
+
 function Hook_CRUD() {
-    const [userDetails,setUserDetails] = useState(initialData);
-    const [list,setList] = useState([]);
-    const [errors,setValidation] = useState({});
+    const [userDetails, setUserDetails] = useState({});
+    const [list, setList] = useState([]);
+    const [isEditable,setEditableIndex] = useState(null);
+    const [showResults, setShowResults] = React.useState(true)
 
-    const onChange = () => {
-
+    const onChange = (event) => {
+        const {name, value,checked} = event.target;
+        if (name === "isActive"){
+            setUserDetails({...userDetails,[name]:checked});
+        }else {
+            setUserDetails({...userDetails, [name]: value});
+        }
     }
-    const handleSubmit = () => {
-        console.log("Submit");
+    //toggle button
+    const onClick = () =>{
+        setShowResults(!showResults);
+        setUserDetails({});
+    }
+
+    const handleSubmit = (event) => {
+        if (isEditable !== null){
+            list[isEditable] = userDetails;
+        }else{
+            setList([...list, userDetails]);
+        }
+        setUserDetails({});
+        setShowResults(!showResults)
+    }
+    const EditData = (index) => {
+        setShowResults(!showResults);
+        setUserDetails(list[index]);
+        setEditableIndex(index);
+    }
+    const DeleteData = (index) => {
+        setList(list.filter((value,i) =>{
+            return i !== index;
+        }));
     }
     return (
-        <>
-            <Col md={12}>
-                <div className="form-div">
-                    <Row className="mt-3">
-                        <Col md={2} className="text-center">First Name</Col>
-                        <Col md={8}>
-                            <input
-                                type="text"
-                                className="form-control form-control-lg"
-                                placeholder="First Name"
-                                // value={name}
-                                name="firstName"
-                                onChange={onChange}
-                                maxLength={100}
-                            />
-                            {/*<span className="text-danger">{errors.Name}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-3">
-                        <Col md={2} className="text-center">Last Name</Col>
-                        <Col md={8}>
-                            <input
-                                type="text"
-                                className="form-control form-control-lg"
-                                placeholder="Last Name"
-                                // value={organization.Name}
-                                name="lastName"
-                                onChange={onChange}
-                                maxLength={100}
-                            />
-                            {/*<span className="text-danger">{errors.Name}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col md={2} className="text-center">Age</Col>
-                        <Col md={8}>
-                            <input
-                                type="text"
-                                className="form-control form-control-lg"
-                                placeholder="Age"
-                                // value={organization.Abbrevlation}
-                                name="Age"
-                                onChange={onChange}
-                                maxLength={100}
-                            />
-                            {/*<span className="text-danger">{errors.Abbrevlation}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col md={2} className="text-center">Gender</Col>
-                        <Col md={8}>
-                            <div className="form-check">
-                                <input type="radio" className="form-check-input" onChange={onChange}
-                                       name="gender" value="Male"/>
-                                {/*checked={organization.AuthorAnonymous === "Post anonymously"}*/}
-                                <label className="form-check-label" htmlFor="gender1">Male</label>
-                            </div>
-                            <div className="form-check">
-                                <input type="radio" className="form-check-input" onChange={onChange}
-                                       name="gender" value="Female"/>
-                                {/*checked={organization.AuthorAnonymous === "Post as myself"}*/}
-                                <label className="form-check-label" htmlFor="gender2">Female</label>
-                            </div>
-                            <div className="form-check">
-                                <input type="radio" className="form-check-input" onChange={onChange}
-                                       name="gender" value="Other"/>
-                                {/*checked={organization.AuthorAnonymous === "Post as myself"}*/}
-                                <label className="form-check-label" htmlFor="gender3">Other</label>
-                            </div>
-                            {/*<span className="text-danger">{errors.AuthorAnonymous}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col className="d-flex" md={2} className="text-center">Address</Col>
-                        <Col md={8}>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                            {/*<span className="text-danger">{errors.Description_Short}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col md={2} className="text-center">Country</Col>
-                        <Col md={8}>
-                            <select
-                                className="form-control"
-                                style={{width: '100%'}}
-                                name="country"
-                                placeholder="Please select..."
-                                // value={organization.Nr_Employees || ""}
-                                onChange={onChange}
-                            >
-                                {numOfCountries.map((numOfCountries, index) => <option key={numOfCountries.value}
-                                                                                       disabled={index === 0}
-                                                                                       value={numOfCountries.value}>{numOfCountries.label}</option>)}
-                            </select>
-                            {/*<span className="text-danger">{errors.Nr_Employees}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2">
-                        <Col md={2}/>
-                        <Col md={8}>
-                            <input
-                                type="checkbox"
-                                // value={organization.Contact_Email}
-                                name="isActive"
-                                onChange={onChange}
-                            />
-                            <label className="form-check-label" htmlFor="active" className="ml-3 mt-2">is Active</label>
-                            {/*<span className="text-danger">{errors.Contact_Email}</span>*/}
-                        </Col>
-                    </Row>
-                    <Row className="mt-2 text-right">
-                        <Col md={11}>
-                            <Button variant="contained" color="primary" component="span" onClick={handleSubmit}>
-                                Submit
-                            </Button>
-                        </Col>
-                    </Row>
+    <>
+        <div className="container">
+            <button className="btn-primary" type="button" onClick={onClick}>Add New</button>
+            <div className="row">
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                        <tr className="table-striped">
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {Array.isArray(list) && list.map((item, index) => {
+                            return (
+                                <tr key={index} id={index}>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.age}</td>
+                                    <td>
+                                        <Button variant="outlined" color="primary" href="#outlined-buttons"
+                                                onClick={() => {
+                                                    EditData(index)
+                                                }}>
+                                            <EditIcon/>
+                                        </Button>
+                                        &nbsp;&nbsp;
+                                        <Button variant="outlined" color="secondary" onClick={() => {
+                                            DeleteData(index)
+                                        }}>
+                                            <DeleteIcon/>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+                        </tbody>
+                    </table>
+                    <br/>
                 </div>
-                <Row className="mt-5 text-center">
-                    <Col md={12}>
-                        <Col md={3}><h2>List</h2></Col>
-                        <table>
-                            <thead>
-                            <tr>
-                                <td>Id</td>
-                                <td>Name</td>
-                                <td>Abbrevlation</td>
-                                <td>Email</td>
-                                <td>Website</td>
-                                <td>Employees</td>
-                                <td>Revenue</td>
-                                <td>Profit</td>
-                                <td>Post</td>
-                                <td>Action</td>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {/*{Array.isArray(list) && list.map((item, index) => {*/}
-                            {/*    return (*/}
-                            {/*        <tr key={index} id={index}>*/}
-                            {/*            <td>{index + 1}</td>*/}
-                            {/*            <td>{item.Name}</td>*/}
-                            {/*            <td>{item.Abbrevlation}</td>*/}
-                            {/*            <td>{item.Contact_Email}</td>*/}
-                            {/*            <td>{item.Website}</td>*/}
-                            {/*            <td>{item.Nr_Employees}</td>*/}
-                            {/*            <td>{item.Revenue}</td>*/}
-                            {/*            <td>{item.Profit}</td>*/}
-                            {/*            <td>{item.AuthorAnonymous}</td>*/}
-                            {/*            <td>*/}
-                            {/*                <Button variant="outlined" color="primary" href="#outlined-buttons"*/}
-                            {/*                        onClick={() => {*/}
-                            {/*                            this.EditData(index)*/}
-                            {/*                        }}>*/}
-                            {/*                    <EditIcon/>*/}
-                            {/*                </Button>*/}
-                            {/*                &nbsp;&nbsp;*/}
-                            {/*                <Button variant="outlined" color="secondary" onClick={() => {*/}
-                            {/*                    this.DeleteData(index)*/}
-                            {/*                }}>*/}
-                            {/*                    <DeleteIcon/>*/}
-                            {/*                </Button>*/}
-                            {/*            </td>*/}
-                            {/*        </tr>*/}
-                            {/*    )*/}
-                            {/*})*/}
-                            {/*}*/}
-                            </tbody>
-                        </table>
-                    </Col>
-                </Row>
-            </Col>
-        </>
-    );
+            </div>
+            { showResults && <div className="row>">
+
+                <div className="col-md-6">
+                    <h2>Registration Form</h2> <br/>
+                    First name: <br/>
+                    <input className="form-control" name="firstName" type="text"  value={userDetails.firstName || ""}
+                           placeholder="Enter Your First Name" onChange={onChange}/><br/>
+                    Last name: <br/>
+                    <input className="form-control" name="lastName" type="text" value={userDetails.lastName || ""}
+                           placeholder="Enter Your Last Name" onChange={onChange}/><br/>
+                    Age :<br/>
+                    <input className="form-control" type="text" name="age" value={userDetails.age || ""}
+                           placeholder="Enter Your Age" onChange={onChange}/><br/>
+                    Gender : <br/>
+                    <input type="radio" name="gender" value="Male" checked={userDetails.gender === "Male"}
+                           onChange={onChange}/>Male
+                    <input type="radio" name="gender" value="Female" checked={userDetails.gender === "Female"}
+                           onChange={onChange}/>Female
+                    <input type="radio" name="gender" value="Others" checked={userDetails.gender === "Others"}
+                           onChange={onChange}/>Others<br/><br/>
+                    Address : <br/>
+                    <textarea className="form-control" cols="25" rows="4" name="address"
+                              value={userDetails.address || ""} placeholder="Enter Your Address" onChange={onChange}>
+                </textarea>
+                    Country : <br/>
+                    <select className="form-control" name="country" id="country" value={userDetails.country || ""}
+                            required onChange={onChange}>
+                        {numOfCountries.map((numOfCountry, index) => <option key={numOfCountry.value}
+                                                                             disabled={index === 0}
+                                                                             value={numOfCountry.value}>{numOfCountry.label}</option>)}
+                    </select><br/>
+                    <input type="checkbox" name="isActive" value="I Agree the Terms & Conditions" checked={userDetails.isActive} onChange={onChange}/>&nbsp;
+                    <label>I Agree the Terms & Conditions</label><br/>
+                    <Button variant="contained" color="primary" component="span" onClick={handleSubmit}>Submit</Button>
+                </div>
+            </div> }
+        </div>
+    </>
+)
 }
 
 export default Hook_CRUD;
